8d7b531a3eb4cd721b91acc7213f8c8b800c4102
Nelson Kane <37565271+nellyBoi@users.noreply.github.com>
2019-10-27 22:29:45 -0600

    Add files via upload

---
+++ ip_view_ui/ipview_ui.py
@@ -0,0 +1,100 @@
+"""
+File to import auto-formatted *base.py file.
+"""
+from PyQt5 import QtGui
+from PyQt5 import QtWidgets
+from PyQt5.QtCore import Qt
+from PyQt5.QtGui import QPixmap
+from PyQt5.QtWidgets import QGraphicsScene
+
+from events import Events
+from ipview_ui_base import Ui_MainWindow  # importing our generated file
+
+
+########################################################################################################################
+class MyWindow(QtWidgets.QMainWindow):
+
+    ####################################################################################################################
+    def __init__(self):
+
+        # instantiate events that holds and handles non-ui related data
+        self.events = Events()
+
+        super(MyWindow, self).__init__()
+        self.ui = Ui_MainWindow()
+        self.ui.setupUi(self)
+        self.setWindowTitle("IP View")
+
+        # set color of main window background
+        self.setAutoFillBackground(True)
+        p = self.palette()
+        p.setColor(self.backgroundRole(), Qt.white)
+        self.setPalette(p)
+
+        # modify text directory input box
+        self.__modify_directory_input()
+
+        # connecting signals to slop methods
+        self.ui.directory_load_push.clicked.connect(self.load_directory_button_pushed)
+        self.ui.clear_push_button.clicked.connect(self.clear_button_pushed)
+        self.ui.next_button.clicked.connect(self.next_button_pushed)
+
+    ####################################################################################################################
+    def __display_image(self) -> None:
+        """
+        """
+        x = self.ui.image_display.x()
+        y = self.ui.image_display.y()
+        w = self.ui.image_display.width()
+        h = self.ui.image_display.height()
+        scene = QGraphicsScene()
+
+        im = self.events.get_next_image()
+        if im is not None:
+
+            im = im.scaled(w, h, Qt.KeepAspectRatio, Qt.FastTransformation)
+            scene.addPixmap(QPixmap.fromImage(im))
+            self.ui.image_display.setScene(scene)
+            self.ui.image_display.show()
+
+    ####################################################################################################################
+    def __modify_directory_input(self) -> None:
+        """
+        """
+        self.ui.directory_input.setText(r"C:\Nelly\IPView\image_containers\data\images")
+        self.ui.directory_input.setFont(QtGui.QFont('SansSerif', 12))
+        self.ui.directory_input.setStyleSheet("color: rgb(28, 43, 255);")  # change text color
+
+    ####################################################################################################################
+    def load_directory_button_pushed(self) -> None:
+        """
+        Slot method for loading directory upon load push button.
+        """
+        directory = self.ui.directory_input.toPlainText()
+        files_for_display = self.events.load_directory(directory=directory)
+        if len(files_for_display) == 0:
+            self.ui.text_list_display.setText('No Compatible Images')
+        else:
+            string = '\n'.join(map(str, files_for_display))
+            self.ui.text_list_display.setText(string)
+
+        return
+
+    ####################################################################################################################
+    def next_button_pushed(self) -> None:
+        """
+        Slot method for a signal from the next push button.
+        """
+        self.__display_image()
+
+    ####################################################################################################################
+    def clear_button_pushed(self) -> None:
+        """
+        Slot method for clearing all data from the UI and from memory.
+        """
+        self.events.clear_data()
+        self.ui.text_list_display.setText('')
+
+        return
+
+
---
+++ ip_view_ui/ipview_ui_base.py
@@ -0,0 +1,55 @@
+# -*- coding: utf-8 -*-
+
+# Form implementation generated from reading ui file 'ipview_base.ui'
+#
+# Created by: PyQt5 UI code generator 5.13.0
+#
+# WARNING! All changes made in this file will be lost!
+
+
+from PyQt5 import QtCore, QtGui, QtWidgets
+
+
+class Ui_MainWindow(object):
+    def setupUi(self, MainWindow):
+        MainWindow.setObjectName("MainWindow")
+        MainWindow.resize(826, 600)
+        self.centralwidget = QtWidgets.QWidget(MainWindow)
+        self.centralwidget.setObjectName("centralwidget")
+        self.directory_input = QtWidgets.QTextEdit(self.centralwidget)
+        self.directory_input.setGeometry(QtCore.QRect(530, 30, 256, 41))
+        self.directory_input.setObjectName("directory_input")
+        self.text_list_display = QtWidgets.QTextEdit(self.centralwidget)
+        self.text_list_display.setGeometry(QtCore.QRect(530, 130, 256, 192))
+        self.text_list_display.setObjectName("text_list_display")
+        self.directory_load_push = QtWidgets.QPushButton(self.centralwidget)
+        self.directory_load_push.setGeometry(QtCore.QRect(710, 90, 75, 23))
+        self.directory_load_push.setObjectName("directory_load_push")
+        self.clear_push_button = QtWidgets.QPushButton(self.centralwidget)
+        self.clear_push_button.setGeometry(QtCore.QRect(710, 340, 75, 23))
+        self.clear_push_button.setObjectName("clear_push_button")
+        self.image_display = QtWidgets.QGraphicsView(self.centralwidget)
+        self.image_display.setGeometry(QtCore.QRect(20, 30, 471, 441))
+        self.image_display.setObjectName("image_display")
+        self.next_button = QtWidgets.QPushButton(self.centralwidget)
+        self.next_button.setGeometry(QtCore.QRect(550, 90, 75, 23))
+        self.next_button.setObjectName("next_button")
+        MainWindow.setCentralWidget(self.centralwidget)
+        self.menubar = QtWidgets.QMenuBar(MainWindow)
+        self.menubar.setGeometry(QtCore.QRect(0, 0, 826, 22))
+        self.menubar.setObjectName("menubar")
+        MainWindow.setMenuBar(self.menubar)
+        self.statusbar = QtWidgets.QStatusBar(MainWindow)
+        self.statusbar.setObjectName("statusbar")
+        MainWindow.setStatusBar(self.statusbar)
+
+        self.retranslateUi(MainWindow)
+        QtCore.QMetaObject.connectSlotsByName(MainWindow)
+
+    def retranslateUi(self, MainWindow):
+        _translate = QtCore.QCoreApplication.translate
+        MainWindow.setWindowTitle(_translate("MainWindow", "MainWindow"))
+        self.directory_input.setWhatsThis(_translate("MainWindow", "<html><head/><body><p><br/></p></body></html>"))
+        self.directory_load_push.setText(_translate("MainWindow", "Load"))
+        self.clear_push_button.setText(_translate("MainWindow", "Clear"))
+        self.next_button.setText(_translate("MainWindow", "next"))
