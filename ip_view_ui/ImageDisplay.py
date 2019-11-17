2de123e9e06a10df7fefb9e05cf78e0ff987a549
John Kane <john.n.kane@colorado.edu>
2019-11-17 13:42:44 -0700

    progress on crop

--- ip_view_ui/ImageDisplay.py
+++ ip_view_ui/ImageDisplay.py
@@ -2,9 +2,9 @@
 Nelly Kane
 11.12.2019
 """
-from PyQt5.QtCore import (Qt, QRectF, QRect, QSize, QPoint, pyqtSignal)
+from PyQt5.QtCore import (Qt, QRectF, QRect, QSize, QPoint)
 from PyQt5.QtGui import (QPixmap, QPainterPath)
-from PyQt5.QtWidgets import (QGraphicsScene, QRubberBand, QGraphicsView, QGraphicsSceneMouseEvent)
+from PyQt5.QtWidgets import (QGraphicsScene, QRubberBand, QGraphicsSceneMouseEvent)
 
 import StreamDisplay
 import image as im
@@ -18,10 +18,6 @@ class ImageDisplay(QGraphicsScene):
     """
     CROP_IMAGE = Qt.LeftButton
     PIXEL_FETCH = Qt.RightButton
-
-    # Mouse button signals emit image scene (x, y) coordinates. row = y and column = x.
-    leftMouseButtonPressed = pyqtSignal(float, float)
-    leftMouseButtonReleased = pyqtSignal(float, float)
 
     ####################################################################################################################
     def __init__(self,
@@ -39,8 +35,11 @@ class ImageDisplay(QGraphicsScene):
         self.__displayed_image_orig = None
 
         # data members reserved for mouse events and the sharing of data between override methods.
-        self.__first_press_pos = None
+        self.__orig_scene_pos = None
+        self.__orig_screen_pos = None
         self.__current_rubber_band = None
+        self.__cropped_image_rect = None
+        self.__button_type = None
 
         self.stream_display = StreamDisplay.StreamDisplay(ui=self.ui)
 
@@ -74,7 +73,9 @@ class ImageDisplay(QGraphicsScene):
         self.clear()
         self.ui.image_display.show()
         self.__displayed_image = None  # reset image held in object
-        self.__displayed_image_orig = None # reset original image held in object
+        self.__displayed_image_orig = None  # reset original image held in object
+        self.__cropped_image_rect = None
+        self.__button_type = None
 
         return
 
@@ -95,8 +96,9 @@ class ImageDisplay(QGraphicsScene):
         if self.__displayed_image is None:
             return
 
-        x = event.scenePos().x()
-        y = event.scenePos().y()
+        xy = event.scenePos().toPoint()
+        x = xy.x()
+        y = xy.y()
 
         height = self.ui.image_display.sceneRect().height()
         width = self.ui.image_display.sceneRect().width()
@@ -106,18 +108,15 @@ class ImageDisplay(QGraphicsScene):
             return
 
         if event.button() == ImageDisplay.CROP_IMAGE:
-
-            self.__first_press_pos = QPoint(int(x), int(y))
-            self.__current_rubber_band = QRubberBand(QRubberBand.Rectangle)
-            self.__current_rubber_band.setGeometry(QRect(QPoint(self.__first_press_pos), QSize()))
+            self.__button_type = ImageDisplay.CROP_IMAGE  # TODO Do we need this?
+            self.__orig_scene_pos = xy
+            self.__orig_screen_pos = event.screenPos()
+            self.__current_rubber_band = QRubberBand(QRubberBand.Rectangle, self.parent())
+            self.__current_rubber_band.setGeometry(QRect(self.__orig_screen_pos, self.__orig_screen_pos))
             self.__current_rubber_band.show()
-            """
-            self.__q_graphics_view.setDragMode(QGraphicsView.RubberBandDrag)
-            self.leftMouseButtonPressed.emit(x, y)
-            #QGraphicsView.mousePressEvent(self, event)
-            """
 
         elif event.button() == ImageDisplay.PIXEL_FETCH:
+            self.__button_type = ImageDisplay.PIXEL_FETCH
             self.stream_display.append_row('Pixel: row[{0:.2f}], col[{1:.2f}]'.format(y, x))
 
         return
@@ -127,9 +126,11 @@ class ImageDisplay(QGraphicsScene):
         """
         Override method to move rubber band while mouse button is pressed.
         """
-        xy = event.scenePos()
-        pos_int = QPoint(int(xy.x()), int(xy.y()))
-        self.__current_rubber_band.setGeometry(QRect(self.__first_press_pos, pos_int))
+        event_type = self.__button_type
+        if event_type == ImageDisplay.CROP_IMAGE:
+            xy = event.screenPos()
+            pos_int = QPoint(int(xy.x()), int(xy.y()))
+            self.__current_rubber_band.setGeometry(QRect(self.__orig_screen_pos, pos_int))
 
         return
 
@@ -141,31 +142,25 @@ class ImageDisplay(QGraphicsScene):
         if event.button() == ImageDisplay.CROP_IMAGE:
 
             self.__current_rubber_band.hide()
-            current_rect = self.__current_rubber_band.geometry()
+            self.__cropped_image_rect = QRect(self.__orig_scene_pos, event.scenePos().toPoint())
+            self.__crop_info_to_stream()
             self.__current_rubber_band.deleteLater()
-            cropped_image = self.__displayed_image.copy(current_rect)
+            cropped_image = self.__displayed_image.copy(self.__cropped_image_rect)
             self.__display_image(image=cropped_image)
-            """
-            self.__q_graphics_view.
-            view_box = self.zoomStack[-1] if len(self.zoomStack) else self.sceneRect()
-            selection_box = self.scene.selectionArea().boundingRect().intersected(view_box)
-            self.setSelectionArea(QPainterPath)
-            self.scene.setSelectionArea(QPainterPath())  # Clear current selection area.
-            if selectionBBox.isValid() and (selectionBBox != viewBBox):
-                self.zoomStack.append(selectionBBox)
-                self.updateViewer()
-            self.setDragMode(QGraphicsView.NoDrag)
-            self.rightMouseButtonReleased.emit(scenePos.x(), scenePos.y())
-            """
 
         return
 
     ####################################################################################################################
-    def mouseDoubleClickEvent(self, event: 'QGraphicsSceneMouseEvent'):
+    def mouseDoubleClickEvent(self, event: QGraphicsSceneMouseEvent):
         """
         Override method to display original image.
         """
-        pass
+        if event.button() == ImageDisplay.CROP_IMAGE: # TODO :: Maybe revert should be a button instead??
+            self.__display_image(image=self.__displayed_image_orig)
+            self.stream_display.clear_text()
+            self.stream_display.append_row("Image reverted to original")
+
+        return
 
     ####################################################################################################################
     def __display_image(self, image: im.Image) -> None:
@@ -182,3 +177,11 @@ class ImageDisplay(QGraphicsScene):
             self.ui.image_display.show()
 
         return
+
+    ####################################################################################################################
+    def __crop_info_to_stream(self) -> None:
+        """
+        Method to write image crop information to stream.
+        """
+        self.stream_display.append_row('Image Cropped, New Parameters: ')
+        self.stream_display.append_row(str(self.__cropped_image_rect))
